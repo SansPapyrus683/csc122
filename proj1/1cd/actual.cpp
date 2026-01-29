c2df701400c907e160461cd8b02ffb44dd1f8d12
Kevin Sheng <kevinsheng2005@gmail.com>
2026-01-29 13:20:33 -0800

    slightly better options for 1c/1d

--- proj1/1cd/actual.cpp
+++ proj1/1cd/actual.cpp
@@ -10,18 +10,18 @@
 
 using namespace std;
 
-const string PROJ = "c";
-const vector<int> USE{24, 49, 64, 94, 1, 37, 67, 42, 78, 14};
-// const string PROJ = "d";
-// const vector<int> USE{965,  3767, 2664, 1972, 2307, 1137, 2052, 388,  1172, 4258,
-//                       2463, 2413, 1945, 4119, 4363, 4325, 2103, 2911, 593,  2373,
-//                       4689, 1372, 2367, 2881, 2251, 2016, 2932, 3144, 4353, 971,
-//                       4556, 2900, 1624, 1792, 4626, 209,  777,  2083, 341,  969,
-//                       2975, 4442, 4080, 4708, 4892, 4480, 683,  119,  1141};
+// const string PROJ = "c";
+// const vector<int> USE{24, 49, 64, 94, 1, 37, 67, 42, 78, 14};
+const string PROJ = "d";
+const vector<int> USE{965,  3767, 2664, 1972, 2307, 1137, 2052, 388,  1172, 4258,
+                      2463, 2413, 1945, 4119, 4363, 4325, 2103, 2911, 593,  2373,
+                      4689, 1372, 2367, 2881, 2251, 2016, 2932, 3144, 4353, 971,
+                      4556, 2900, 1624, 1792, 4626, 209,  777,  2083, 341,  969,
+                      2975, 4442, 4080, 4708, 4892, 4480, 683,  119,  1141};
 
-constexpr int KMER_LEN = 20;
-constexpr int HASH_FUNCS = 10;
-constexpr long long MOD = 8986687;
+constexpr int KMER_LEN = 15;
+constexpr int HASH_FUNCS = 20;
+constexpr long long MOD = 1e9 + 7;
 
 int main() {
     hashing_init(KMER_LEN, HASH_FUNCS, MOD);
